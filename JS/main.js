647f3cc904f708062aadd67d0ed9e2cd5f8c3fca
gksucalvi <106171918+gksucalvi@users.noreply.github.com>
2023-05-09 14:52:39 -0300

    arrumando ortografia

--- JS/main.js
+++ JS/main.js
@@ -4,5 +4,5 @@ form.addEventListener("submit", (evento) =>{
     evento.preventDefault()
     console.log(evento)
     console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['uantidade'].value)
+    console.log(evento.target.elements['quantidade'].value)
 })
