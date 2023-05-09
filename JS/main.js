5fd705d1ae275eb39d8913912eda6c7cd7bf26d9
gksucalvi <106171918+gksucalvi@users.noreply.github.com>
2023-05-09 14:50:10 -0300

    09/05

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+const form = documents.getElementById("novoItem")
+
+form.addEventListener("submit", (evento) =>{
+    evento.preventDefault()
+    console.log(evento)
+    console.log(evento.target.elements['nome'].value)
+    console.log(evento.target.elements['uantidade'].value)
+})
